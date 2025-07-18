cdec87115f1cf1e4247e043db6712aecfa832fb7
Aurelia Wang <aurelia.wang@glean.com>
2025-07-17 17:55:29 -0700

    remove search_result from list

--- scripts/create_indexing_config.py
+++ scripts/create_indexing_config.py
@@ -26,11 +26,11 @@ def main():
                     display_label="Information Page",
                     doc_category=models.DatasourceCategory.KNOWLEDGE_HUB,
                     property_definitions=[
-                        models.PropertyDefinition(name="title", display_label="Title", property_type=models.PropertyType.TEXT, ui_options=[models.UIOptions.SEARCH_RESULT], hide_ui_facet=True),
-                        models.PropertyDefinition(name="url", display_label="URL", property_type=models.PropertyType.TEXT, ui_options=[models.UIOptions.SEARCH_RESULT], hide_ui_facet=True),
-                        models.PropertyDefinition(name="section", display_label="Section", property_type=models.PropertyType.TEXT, ui_options=[models.UIOptions.SEARCH_RESULT]),
-                        models.PropertyDefinition(name="heading", display_label="Heading", property_type=models.PropertyType.TEXT, ui_options=[models.UIOptions.SEARCH_RESULT], hide_ui_facet=True),
-                        models.PropertyDefinition(name="content", display_label="Content", property_type=models.PropertyType.TEXT, ui_options=[models.UIOptions.SEARCH_RESULT], hide_ui_facet=True),
+                        models.PropertyDefinition(name="title", display_label="Title", property_type=models.PropertyType.TEXT, ui_options=models.UIOptions.SEARCH_RESULT, hide_ui_facet=True),
+                        models.PropertyDefinition(name="url", display_label="URL", property_type=models.PropertyType.TEXT, ui_options=models.UIOptions.SEARCH_RESULT, hide_ui_facet=True),
+                        models.PropertyDefinition(name="section", display_label="Section", property_type=models.PropertyType.TEXT, ui_options=models.UIOptions.SEARCH_RESULT),
+                        models.PropertyDefinition(name="heading", display_label="Heading", property_type=models.PropertyType.TEXT, ui_options=models.UIOptions.SEARCH_RESULT, hide_ui_facet=True),
+                        models.PropertyDefinition(name="content", display_label="Content", property_type=models.PropertyType.TEXT, ui_options=models.UIOptions.SEARCH_RESULT, hide_ui_facet=True),
                     ]
                 ),
                 models.ObjectDefinition(
@@ -42,99 +42,99 @@ def main():
                             name="title",
                             display_label="Title",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT],
+                            ui_options=models.UIOptions.SEARCH_RESULT,
                             hide_ui_facet=True
                         ),
                         models.PropertyDefinition(
                             name="tag",
                             display_label="API Tag",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT]
+                            ui_options=models.UIOptions.SEARCH_RESULT
                         ),
                         models.PropertyDefinition(
                             name="endpoint",
                             display_label="Endpoint",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT],
+                            ui_options=models.UIOptions.SEARCH_RESULT,
                             hide_ui_facet=True
                         ),
                         models.PropertyDefinition(
                             name="url",
                             display_label="Full URL",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT],
+                            ui_options=models.UIOptions.SEARCH_RESULT,
                             hide_ui_facet=True
                         ),
                         models.PropertyDefinition(
                             name="method",
                             display_label="HTTP Method",
                             property_type=models.PropertyType.PICKLIST,
-                            ui_options=[models.UIOptions.SEARCH_RESULT],
+                            ui_options=models.UIOptions.SEARCH_RESULT,
                         ),
                         models.PropertyDefinition(
                             name="description",
                             display_label="Description",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT],
+                            ui_options=models.UIOptions.SEARCH_RESULT,
                             hide_ui_facet=True
                         ),
                         models.PropertyDefinition(
                             name="request_content_type",
                             display_label="Request Content Type",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT]
+                            ui_options=models.UIOptions.SEARCH_RESULT
                         ),
                         models.PropertyDefinition(
                             name="request_parameters",
                             display_label="Request Parameters",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT],
+                            ui_options=models.UIOptions.SEARCH_RESULT,
                             hide_ui_facet=True
                         ),
                         models.PropertyDefinition(
                             name="request_body",
                             display_label="Request Body",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT],
+                            ui_options=models.UIOptions.SEARCH_RESULT,
                             hide_ui_facet=True
                         ),
                         models.PropertyDefinition(
                             name="response_content_type",
                             display_label="Response Content Type",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT]
+                            ui_options=models.UIOptions.SEARCH_RESULT
                         ),
                         models.PropertyDefinition(
                             name="response",
                             display_label="Response",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT],
+                            ui_options=models.UIOptions.SEARCH_RESULT,
                             hide_ui_facet=True
                         ),
                         models.PropertyDefinition(
                             name="response_codes",
                             display_label="Response Codes",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT]
+                            ui_options=models.UIOptions.SEARCH_RESULT
                         ),
                         models.PropertyDefinition(
                             name="authentication",
                             display_label="Authentication",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT]
+                            ui_options=models.UIOptions.SEARCH_RESULT
                         ),
                         models.PropertyDefinition(
                             name="code_examples",
                             display_label="Code Examples",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT],
+                            ui_options=models.UIOptions.SEARCH_RESULT,
                             hide_ui_facet=True
                         ),
                         models.PropertyDefinition(
                             name="supported_languages",
                             display_label="Supported Languages",
                             property_type=models.PropertyType.TEXT,
-                            ui_options=[models.UIOptions.SEARCH_RESULT]
+                            ui_options=models.UIOptions.SEARCH_RESULT
                         ),
                     ]
                 )
