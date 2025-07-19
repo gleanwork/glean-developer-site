bbecc573bd79e22122cb582770212266c48a5d39
Aurelia Wang <aurelia.wang@glean.com>
2025-07-18 17:10:17 -0700

    consolidate into one data client

--- scripts/indexing/data_clients.py
+++ scripts/indexing/data_clients.py
@@ -3,25 +3,9 @@ from typing import Sequence, Union
 from glean.indexing.connectors.base_data_client import BaseConnectorDataClient
 from data_types import PageInfoData, ApiReferenceData
 
-class InfoPageDataClient(BaseConnectorDataClient[PageInfoData]):
-    def __init__(self):
-        pass
-
-    def get_source_data(self, since=None) -> Sequence[PageInfoData]:
-        return []
-
-class ApiReferenceDataClient(BaseConnectorDataClient[ApiReferenceData]):
-    def __init__(self):
-        pass
-
-    def get_source_data(self, since=None) -> Sequence[ApiReferenceData]:
-        return []
-
-class CompositeDataClient(BaseConnectorDataClient[Union[PageInfoData, ApiReferenceData]]):
-    def __init__(self, info_client: InfoPageDataClient, api_client: ApiReferenceDataClient, dev_docs_base_url: str):
-        self.info_client = info_client
-        self.api_client = api_client
+class DeveloperDocsDataClient(BaseConnectorDataClient[Union[PageInfoData, ApiReferenceData]]):
+    def __init__(self, dev_docs_base_url: str):
         self.dev_docs_base_url = dev_docs_base_url
 
     def get_source_data(self, since=None) -> Sequence[Union[PageInfoData, ApiReferenceData]]:
-        return list(self.info_client.get_source_data(since)) + list(self.api_client.get_source_data(since))
+        return []
--- scripts/indexing/main.py
+++ scripts/indexing/main.py
@@ -5,13 +5,11 @@
 # ]
 # ///
 
-from data_clients import InfoPageDataClient, ApiReferenceDataClient, CompositeDataClient
+from data_clients import DeveloperDocsDataClient
 from developer_docs_connector import CustomDeveloperDocsConnector
 from glean.indexing.models import IndexingMode
 
-info_client = InfoPageDataClient()
-api_client = ApiReferenceDataClient()
-composite_client = CompositeDataClient(info_client, api_client, "https://developers.glean.com")
+composite_client = DeveloperDocsDataClient("https://developers.glean.com")
 
 connector = CustomDeveloperDocsConnector(name="customDeveloperDocsDatasource", data_client=composite_client)
 connector.configure_datasource(is_test=True)
