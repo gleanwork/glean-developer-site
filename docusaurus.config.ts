b0eeeded9dc465223965453715469f28b942dc02
Aurelia Wang <aurelia.wang@glean.com>
2025-08-07 09:37:50 -0700

    add gleandocs datasource (#95)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -193,7 +193,7 @@ const config: Config = {
         searchOptions: {
           backend: 'https://glean-public-external-be.glean.com',
           webAppUrl: 'https://glean-public-external.glean.com',
-          datasourcesFilter: ['devdocs'],
+          datasourcesFilter: ['devdocs', 'gleandocs'],
         },
         chatOptions: false,
         enableAnonymousAuth: true,
