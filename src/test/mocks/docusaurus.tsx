fbc6a4b1aac37d5d6f37cfced6f85e4c2e11eccc
Steve Calvert <steve.calvert@glean.com>
2025-08-29 12:12:27 -0700

    Removing unnecessary comments

--- src/test/mocks/docusaurus.tsx
+++ src/test/mocks/docusaurus.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { vi } from 'vitest';
 
-// Mock FeatureFlagsContext from @site/src/theme/Root
 vi.mock('@site/src/theme/Root', () => ({
   FeatureFlagsContext: React.createContext({
     isEnabled: () => false,
