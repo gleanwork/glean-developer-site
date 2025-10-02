1bb8d4664c9d135f201cdeec1c02ac06ada60906
Chris Freeman <chris.freeman@glean.com>
2025-10-02 13:37:40 -0600

    chore: fix prettier formatting and ignore auto-generated files

--- src/theme/Schema/index.tsx
+++ src/theme/Schema/index.tsx
@@ -1254,10 +1254,11 @@ const SchemaNode: React.FC<SchemaProps> = ({
 };
 function isEmpty(value: any): boolean {
   if (value == null) return true;
-  if (typeof value === 'string' || Array.isArray(value)) return value.length === 0;
+  if (typeof value === 'string' || Array.isArray(value))
+    return value.length === 0;
   if (value instanceof Map || value instanceof Set) return value.size === 0;
   if (typeof value === 'object') return Object.keys(value).length === 0;
   return false;
-};
+}
 
 export default SchemaNode;
