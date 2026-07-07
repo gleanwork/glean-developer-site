fa624a3c9db9a512f8882d8a0a8ac946da88b5cd
Travis Hoover <travis.hoover@glean.com>
2026-07-07 12:44:10 -0700

    feat(experimental): beaker icon + green/yellow palette for experimental UI (#609)

--- src/theme/Admonition/Types.tsx
+++ src/theme/Admonition/Types.tsx
@@ -2,19 +2,19 @@ import React, { type ComponentProps } from 'react';
 import clsx from 'clsx';
 import DefaultAdmonitionTypes from '@theme-original/Admonition/Types';
 import AdmonitionLayout from '@theme/Admonition/Layout';
-import IconInfo from '@theme/Admonition/Icon/Info';
+import BeakerIcon from '@site/src/components/BeakerIcon';
 
 type AdmonitionProps = ComponentProps<typeof AdmonitionLayout>;
 
 // Custom `experimental` admonition used on API pages for endpoints marked with
-// `x-glean-experimental`. Reuses the Info icon but renders with the orange
+// `x-glean-experimental`. Renders a science-beaker icon with the light-green
 // palette defined for `.theme-admonition-experimental` in src/css/custom.css,
 // matching the "Experimental" sidebar pill.
 function ExperimentalAdmonition(props: AdmonitionProps): React.ReactElement {
   return (
     <AdmonitionLayout
       {...props}
-      icon={props.icon ?? <IconInfo />}
+      icon={props.icon ?? <BeakerIcon />}
       title={props.title ?? 'experimental'}
       className={clsx('alert', props.className)}
     >
